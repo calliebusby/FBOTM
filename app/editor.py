6186a72d56bd0a45228f40fb365025d57f4c7568
CBusby <callie_92@att.net>
2019-01-28 09:45:13 -0500

    Some shadow on the text boiiiiii

--- app/editor.py
+++ app/editor.py
@@ -20,9 +20,15 @@ def append_quote(quote, the_boi):
     # TODO: text border
     # TODO: the width of the page and line break
     meme_font = ImageFont.truetype("Impact.ttf", 12)
+    font_shadow = ImageFont.truetype("Impact.ttf", 12)
     (x, y) = (1, 1)
-    color = 'rgba(255, 255, 255, 1)'
+    color = 'rgb(255, 255, 255)'
+    shadow_black = 'rgb(0, 0, 0)'
 
+    draw.text((x - 2, y - 2), quote, (0, 0, 0), font=meme_font)
+    draw.text((x + 2, y - 2), quote, (0, 0, 0), font=meme_font)
+    draw.text((x + 2, y + 2), quote, (0, 0, 0), font=meme_font)
+    draw.text((x - 2, y + 2), quote, (0, 0, 0), font=meme_font)
     draw.text((x, y), quote, fill=color, font=meme_font)
     image.save('finished_product.jpeg')
     return True
