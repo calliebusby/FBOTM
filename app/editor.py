a4cd50ff0b129a774c5bf6a3d720faf1ce22c39f
CBusby <callie_92@att.net>
2019-01-28 10:15:19 -0500

    moving the text drawing

--- app/editor.py
+++ app/editor.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
 from app import data_manager
-import urllib
 from io import BytesIO
 from urllib.request import Request, urlopen
 
@@ -16,20 +15,9 @@ def append_quote(quote, the_boi):
         b = BytesIO(f.read())
         image = Image.open(b)
 
-    draw = ImageDraw.Draw(image)
-    # TODO: text border
     # TODO: the width of the page and line break
-    meme_font = ImageFont.truetype("Impact.ttf", 12)
-    font_shadow = ImageFont.truetype("Impact.ttf", 12)
-    (x, y) = (1, 1)
-    color = 'rgb(255, 255, 255)'
-    shadow_black = 'rgb(0, 0, 0)'
+    outline_text(image, quote, 1, 1)
 
-    draw.text((x - 2, y - 2), quote, (0, 0, 0), font=meme_font)
-    draw.text((x + 2, y - 2), quote, (0, 0, 0), font=meme_font)
-    draw.text((x + 2, y + 2), quote, (0, 0, 0), font=meme_font)
-    draw.text((x - 2, y + 2), quote, (0, 0, 0), font=meme_font)
-    draw.text((x, y), quote, fill=color, font=meme_font)
     image.save('finished_product.jpeg')
     return True
 
@@ -38,3 +26,20 @@ def select_image(the_boi):
     url = data_manager.find_daily_image(the_boi)
     print(url)
     return url
+
+
+def outline_text(image, quote, x, y):
+    draw = ImageDraw.Draw(image)
+    font_size = calculate_font_size(image)
+    meme_font = ImageFont.truetype("Impact.ttf", 12)
+    white = 'rgb(255, 255, 255)'
+    black = 'rgb(0, 0, 0)'
+    draw.text((x - 2, y - 2), quote, black, font=meme_font)
+    draw.text((x + 2, y - 2), quote, black, font=meme_font)
+    draw.text((x + 2, y + 2), quote, black, font=meme_font)
+    draw.text((x - 2, y + 2), quote, black, font=meme_font)
+    draw.text((x, y), quote, fill=white, font=meme_font)
+
+
+def calculate_font_size(image):
+    return 10
